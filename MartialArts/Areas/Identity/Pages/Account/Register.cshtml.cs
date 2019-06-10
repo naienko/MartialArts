8c817a0b63ef1aaf7b4d95a197a5e5460c21b569
Panya Farnette <naienko@gmail.com>
2019-06-10 14:28:05 -0500

    updated registration

--- MartialArts/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MartialArts/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,7 +81,13 @@ namespace MartialArts.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    Address = Input.Address,
+                    Phone = Input.Phone
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
