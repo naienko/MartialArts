7ddd6bdcc2388ffe4d01d34781dc7f491b706b5f
Panya Farnette <naienko@gmail.com>
2019-06-11 14:36:25 -0500

    class controller scaffolded

---
+++ MartialArts/Controllers/ClassesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MartialArts.Data;
+using MartialArts.Models;
+
+namespace MartialArts.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClassesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Classes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Class.Include(e => e.Style);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Classes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .Include(e => e.Style)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // GET: Classes/Create
+        public IActionResult Create()
+        {
+            ViewData["StyleId"] = new SelectList(_context.Style, "Id", "Name");
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StartTime,EndTime,DayOfWeek,StyleId")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@class);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StyleId"] = new SelectList(_context.Style, "Id", "Name", @class.StyleId);
+            return View(@class);
+        }
+
+        // GET: Classes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+            ViewData["StyleId"] = new SelectList(_context.Style, "Id", "Name", @class.StyleId);
+            return View(@class);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StartTime,EndTime,DayOfWeek,StyleId")] Class @class)
+        {
+            if (id != @class.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@class);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassExists(@class.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StyleId"] = new SelectList(_context.Style, "Id", "Name", @class.StyleId);
+            return View(@class);
+        }
+
+        // GET: Classes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .Include(e => e.Style)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @class = await _context.Class.FindAsync(id);
+            _context.Class.Remove(@class);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassExists(int id)
+        {
+            return _context.Class.Any(e => e.Id == id);
+        }
+    }
+}
