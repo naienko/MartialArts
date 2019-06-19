6d3d3d5b3a3b33bc4f8ced3904de3c4081c89b6e
Panya Farnette <naienko@gmail.com>
2019-06-19 11:57:38 -0500

    index page updated

--- MartialArts/Controllers/HomeController.cs
+++ MartialArts/Controllers/HomeController.cs
@@ -5,14 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MartialArts.Models;
+using MartialArts.Models.ViewModels;
+using MartialArts.Data;
 
 namespace MartialArts.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IndexModel frontPage = new IndexModel
+            {
+                Students = _context.Student.Where(s => s.Active == true).ToList(),
+                Events = _context.Event.Where(e => e.StartTime > DateTime.Now).OrderByDescending(e => e.StartTime).ToList()
+            };
+            return View(frontPage);
         }
 
         public IActionResult Privacy()
---
+++ MartialArts/Models/ViewModels/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MartialArts.Models.ViewModels
+{
+    public class IndexModel
+    {
+        public List<Student> Students { get; set; }
+        public List<Event> Events { get; set; }
+    }
+}
